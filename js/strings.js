df842be4d64cafeb1d6c6aa181335ff844594c92
Jmaine22 <jmaine22@gmail.com>
2021-04-28 14:38:19 -0700

    Assignment10

--- js/strings.js
+++ js/strings.js
@@ -1,8 +1,10 @@
 //STEP 1
-let userName = prompt("What is your name?");
-alert(userName.length);
+// let userName = prompt("What is your name?");
+// alert(userName.length);
 //STEP 2
-
+let userName = prompt("What is your name?");
+let num = prompt("Please enter a number between 1 and 15");
+alert(userName.charAt(num));
 //STEP 3
 
 //STEP 4
