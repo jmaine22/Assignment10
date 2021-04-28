75a4132b99b93bdfd79a22f756edef7f260c203f
Jmaine22 <jmaine22@gmail.com>
2021-04-28 15:06:12 -0700

    assignment10

--- js/strings.js
+++ js/strings.js
@@ -2,11 +2,13 @@
 // let userName = prompt("What is your name?");
 // alert(userName.length);
 //STEP 2
-let userName = prompt("What is your name?");
-let num = parseInt(prompt("Please enter a number between 0 and 15"));
-alert("Letter returned in position " + num + " is " + userName.charAt(num));
+// let userName = prompt("What is your name?");
+// let num = parseInt(prompt("Please enter a number between 0 and 15"));
+// alert("Letter returned in position " + num + " is " + userName.charAt(num));
 //STEP 3
-
+let fName = prompt("What is your first name?");
+let lName = prompt("What is your last name?");
+alert("Your name is: "+ fName.concat(" "+lName));
 //STEP 4
 
 //STEP 5
