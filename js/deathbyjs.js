b2dfda6873c4356b64137542c416ab87a9b6a4e9
Jmaine22 <jmaine22@gmail.com>
2021-04-28 22:53:21 -0700

    assignment10

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,5 +1,10 @@
 //STEP 1
-
+// let alphaOrder = (word) => {
+//     let words = word.split('');
+//     let word1 = words.sort();
+//     console.log(word1);
+// }
+// alphaOrder("music");
 //STEP 2
 
 //STEP 3
