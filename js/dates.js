0296cb68330541378c942529c0c9fda4073997cf
Jmaine22 <jmaine22@gmail.com>
2021-04-28 21:55:04 -0700

    Assignment10

--- js/dates.js
+++ js/dates.js
@@ -4,9 +4,22 @@
 // }
 // console.log(daysInMonth(2, 2021));
 //STEP 2
-
+// let monthsByDate = (dt) => {
+//     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//     return months[dt.getMonth()];
+// };
+// console.log(monthsByDate(new Date("02/15/2021")));
 //STEP 3
-
+// let weekendByDate = (dt) => {
+//     let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//     return weekday[dt.getDay()];
+// };
+// console.log(weekendByDate(new Date("02/20/2021")));
 //STEP 4
-
+// let yestdayDate = new Date();
+// yestdayDate.setDate(yestdayDate.getDate() - 1);
+// console.log(yestdayDate);
 //STEP 5
+let currentDay = new Date();
+currentDay.getDay();
+console.log(currentDay);
