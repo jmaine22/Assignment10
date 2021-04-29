426fe1ce836f51a7298390ad9000c380f77358a5
Jmaine22 <jmaine22@gmail.com>
2021-04-28 23:30:30 -0700

    assignment10

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -6,7 +6,15 @@
 // }
 // alphaOrder("music");
 //STEP 2
-
+let upperfirstLetter = (words) => {
+    for (word in words) {
+        let word = words.split(' ');
+        //word.charAt(0).toUpperCase();
+        console.log(word);
+    }
+    //console.log(newString ,newString.length);
+}
+upperfirstLetter("the news is here");
 //STEP 3
 
 //STEP 4
