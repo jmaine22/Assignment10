064e8757fee8db1ffc797f9883f328dda095e4d0
Jmaine22 <jmaine22@gmail.com>
2021-04-28 19:07:15 -0700

    assignment10

--- js/math.js
+++ js/math.js
@@ -6,7 +6,18 @@
 // let num = parseFloat(prompt("Please enter a decimal number"));
 // console.log(Math.round(num));
 //STEP 3
-
+// let num = parseFloat(prompt("Please enter a decimal number"));
+// console.log(Math.floor(num));
 //STEP 4
-
+// let num = prompt("Please enter five numbers and use a comma after each number");
+// num = num.split(',');
+// num[0] = parseFloat(num[0]);
+// num[1] = parseFloat(num[1]);
+// num[2] = parseFloat(num[2]);
+// num[3] = parseFloat(num[3]);
+// num[4] = parseFloat(num[4]);
+// console.log(num);
+// console.log(Math.max(...num), Math.min(...num));
 //STEP 5
+// let num = parseFloat(prompt("Please enter a number"));
+// alert(Math.sqrt(num));
